832426a5466b3edf194aedbd0e7d861001951d9a
amiransari-d <amiransarinit@gmail.com>
2026-09-16 00:44:18 +0530

    Update socket server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:5000');
+const socket = io.connect('https://abdul-chat-app.onrender.com');
 
 function App() {
   const [username, setUsername] = useState('');
